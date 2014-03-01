9b08e992085a5de3e8f74a8ffcf7a1c599b9cfb2
Vera Kalinichenko <Vera@Veras-MacBook-Air.local>
2014-02-28 22:26:15 -0800

    youtube

--- src/main/java/com/video/recommend/Runner.java
+++ src/main/java/com/video/recommend/Runner.java
@@ -1,48 +1,42 @@
 package com.video.recommend;
 
-
-import java.io.IOException;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.apache.pig.EvalFunc;
-import org.apache.pig.data.Tuple;
-import org.apache.pig.data.TupleFactory;
 
-
-
-public class ExtractVideoTopicUDF extends EvalFunc<String>{
-	TupleFactory mTupleFactory = TupleFactory.getInstance();
-	public String exec(Tuple tuple) throws IOException {
+public class Runner {
+	public static void main(String [] args)
+	{
+		Runner r = new Runner();
+		r.testSomething();
+	}
+	
+	private void testSomething()
+	{
 		String topic = "test";
 		String url_prefix = "http://gdata.youtube.com/feeds/api/videos?part=title&id=";
 		String url_suffix = "";
         // expect price
-        if (tuple == null || tuple.size()<1) {
-        	System.out.println("ExtractVideoTopicUDF: requires one input parameter.");        	
-        }
+        String first = "Velzwi-TUno";
         try {
-            String first = (String)tuple.get(0);
             String uri = url_prefix+first+url_suffix;
             HttpClient httpclient = new DefaultHttpClient();
             HttpGet httpget = new HttpGet(uri);
             HttpResponse response = httpclient.execute(httpget);
             System.out.println(response.getStatusLine().toString());
             HttpEntity entity = response.getEntity();
+            topic = (String)response.getParams().getParameter("title");
+            System.out.println("### = " + topic);
             topic = entity.getContentType().getValue();
-            System.out.println(EntityUtils.toString(entity)); 
-            return topic;
+            System.out.println("### = " + topic);
+            System.out.println(EntityUtils.toString(entity));
         }
-        catch (Exception e) {
-            throw new IOException("ExtractVideoTopicUDF: caught exception processing input.", e);
+        catch(Exception exc) {
+        	System.out.println("uuuu");
         }
-    }
-    		
-    		
-    
+        
+	}
 }
-
